1cd6b4f69130986d4724b67fba64c72f428e240f
Yajun Ge <geyajunyue@gmail.com>
2017-04-15 22:31:26 +0800

    20170415Update

---
+++ HIHOCODER/hihocoder_146week.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int dx[4] = {0, -1, 0, 1};
+int dy[4] = {1, 0, -1, 0};
+
+int n, m, K;
+int need[2000000];
+int ansX, ansY;
+
+void update(int x, int y) 
+{
+	if (ansX == -1) 
+	{
+		ansX = x;
+		ansY = y;
+	} 
+	else if (ansX + ansY > x + y) 
+	{
+		ansX = x;
+		ansY = y;
+	} 
+	else if (ansX + ansY == x + y && x < ansX) 
+	{
+		ansX = x;
+		ansY = y;
+	}
+	
+	return;
+}
+
+long long getsum(int col, int len) 
+{
+	if (len <= col) 
+	{
+		return (1ll + len) * len / 2;
+	}
+	return (1ll + col) * col / 2 + 1ll * (len - col) * col;
+}
+
+void work(int n, int m, bool change) 
+{
+	long long sum = 0;
+	for (int i = 1; i <= m; i++) sum = (sum + getsum(i, n)) % K;
+	for (int start = 1; start <= n; start++) 
+	{
+		int r = (K - sum) % K;
+		if (need[r] != -1) 
+		{
+			int x = 1 + need[r];
+			int y = start + need[r];
+			if (change) swap(x, y);
+			update(x, y);
+		}
+		sum = (sum - getsum(start, n) % K + K + getsum(start + m, n) % K) % K;
+	}
+	
+	return;
+}
+
+int main() 
+{
+	int Q; 
+	cin >> Q;
+	while (Q--) 
+	{
+		cin >> n >> m >> K;
+		memset(need, -1, sizeof(need));
+		
+		int u = 0;
+		need[u] = 0;
+		while (true) 
+		{
+			int nu = (1ll * n * m + u) % K;
+			if (need[nu] != -1) break;
+			need[nu] = need[u] + 1;
+			u = nu;
+		}
+
+		ansX = ansY = -1;
+
+		work(n, m, false);
+		work(m, n, true);
+
+		if (ansX == -1) cout << "-1" << endl;
+		else cout << ansX << " " << ansY << endl;
+	}
+	return 0;
+}
+
